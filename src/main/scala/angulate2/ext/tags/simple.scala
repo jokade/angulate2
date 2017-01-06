26e1244ca2d8969448136eb28eb28f1e4efbc54e
jokade <jokade@karchedon.de>
2017-01-06 07:09:06 +0100

    Closes #51: provide basic scalatags

---
+++ src/main/scala/angulate2/ext/tags/simple.scala
@@ -0,0 +1,90 @@
+//     Project: angulate2 (https://github.com/jokade/angulate2)
+// Description:
+
+// Copyright (c) 2017 Johannes.Kastner <jokade@karchedon.de>
+//               Distributed under the MIT License (see included LICENSE file)
+package angulate2.ext.tags
+
+import scalatags.Text.TypedTag
+import scalatags.Text.all._
+
+object simple extends NgSimpleScalatags
+
+private[angulate2] trait NgSimpleScalatags {
+
+  def tpl(tags: TypedTag[_]*): String = tags map (_.toString) mkString "\n"
+
+  val ngIf = attr("*ngIf", raw = true)
+  val ngFor = attr("*ngFor", raw = true)
+
+  /** One-way binding from data source to view target.
+   *
+   * Use `ngBind("target") := "expression"`
+   * in place of `[target]="expression"`.
+   *
+   * @param target data source
+   */
+  @inline
+  def ngBind(target: String) = attr(s"bind-$target")
+
+  /** Two-way data binding.
+   *
+   * Use `ngBindOn("target") := "expression"`
+   * in place of `[(target)]="expression"`
+   *
+   * @param target binding target
+   */
+  @inline
+  def ngBindOn(target: String) = attr(s"bindon-$target")
+
+  /** Binding to an event listener.
+   *
+   * Use `ngOn("event"):="expression`
+   * in place of `(event)="expression"`
+   *
+   * @param event event name
+   */
+  @inline
+  def ngOn(event: String) = attr(s"on-$target")
+
+  /** Shortcut for `ngOn("click")`
+   */
+  val ngClick = attr("on-click")
+
+  val ngModel = attr("[(ngModel)]", raw = true)
+
+  /** Class binding.
+   *
+   * Use `ngBindClass("cls") := "expression"`
+   * in place of `[class.cls] := "expression"`
+   *
+   * @param cls name of the class
+   */
+  @inline
+  def ngBindClass(cls: String) = attr(s"[class.$cls]", raw = true)
+
+  /** Style binding.
+   *
+   * Use `ngBindStyle("prop"):="expression"`
+   * in place of `[style.prop]="expression"`.
+   *
+   * @param prop name of the CSS style property (e.g. `background`)
+   */
+  @inline
+  def ngBindStyle(prop: String) = attr(s"[style.$prop]", raw = true)
+
+  val ngSwitch = ngBind("ngSwitch")
+  val ngSwitchCase = attr("*ngSwitchCase", raw = true)
+  val ngSwitchDefault = attr("*ngSwitchDefault", raw = true)
+
+  def routerLink(url: String, active: String = "", queryParams: String = "")(xs: Modifier*) =
+    tag("a")(attr("routerLink"):=url, attr("routerLinkActive"):=active, attr("queryParams") := queryParams)(xs)
+  val routerLink = attr("routerLink")
+//  object routerLink {
+//    def :=(url: String) = attr("routerLink") := url
+//  }
+
+  val routerOutlet = tag("router-outlet")
+}
+
+
